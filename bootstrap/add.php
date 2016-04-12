3a9dc54c9d25b268a0897da62f7611bafe7219a8
dreamer2018 <zhoupans_mail@163.com>
2016-04-12 21:13:39 +0800

    add

--- bootstrap/add.php
+++ bootstrap/add.php
@@ -1,8 +1,14 @@
 <?php
 /**
+ *
  * Created by PhpStorm.
  * User: zhoupan
  * Date: 4/9/16
  * Time: 8:42 PM
  */
+    $username='root';
+    $passwd='zhoupan';
+    $host='localhost';
+    mysql_connect();
+
 ?>
