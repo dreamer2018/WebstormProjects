f3b2d2ad7f012d56583358670124b8ea220e1b64
dreamer2018 <zhoupans_mail@163.com>
2016-04-20 09:43:23 +0800

    add

--- bootstrap/add.php
+++ bootstrap/add.php
@@ -6,9 +6,25 @@
  * Date: 4/9/16
  * Time: 8:42 PM
  */
-    $username='root';
-    $passwd='zhoupan';
-    $host='localhost';
-    mysql_connect();
+    $DB_USER='root';
+    $DB_NAME='test';
+    $DB_PASSWD='zhoupan';
+    $DB_HOST='localhost';
+    $DB_PORT="3306";
+    $conn=null;
+    $dsn="mysql:host=".$DB_HOST.";port=".$DB_PORT.";dbname=".$DB_NAME.";charset=utf8";
 
+    try{
+        $conn= new \PDO(
+            $dsn,
+            $DB_USER,
+            $DB_PASSWD,
+            array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+            )
+        );
+    }catch(PDOException $e){
+        echo "数据库连接失败".$e->getMessage();
+        exit();
+    }
 ?>
