5612bf756e889ab7aa8ddf23e90d612eb8ed2d9f
dreamer2018 <zhoupans_mail@163.com>
2016-04-22 21:28:08 +0800

    test

--- bootstrap/test/preg_test.php
+++ bootstrap/test/preg_test.php
@@ -5,12 +5,18 @@
  * Date: 4/22/16
  * Time: 8:20 PM
  */
-$str="12345678911";
+$str="18345668911";
 echo strlen($str);
-if(preg_match("/[1-9]{11}/",$str)){
+if(preg_match("/(13\d|14[57]|15[^4,\D]|17[678]|18\d)\d{8}|170[059]\d{7}/",$str)){
   echo "success!";
 }else{
     echo "error";
+}
+$name="周攀";
+if(preg_match('/^[\x{4e00}-\x{9fa5}]+$/u',$name)){
+    echo "IS";
+}else{
+    echo "NOT";
 }
 
 ?>
