74323101451a1cea7d728684343eb1177fea444c
dreamer2018 <zhoupans_mail@163.com>
2016-04-24 11:13:43 +0800

    将页面作部分改变

---
+++ ClothesCount/DB_login.php
@@ -0,0 +1,6 @@
+<?php
+$DB_USER = 'root';
+$DB_PASSWD = 'zhoupan';
+$DB_HOST = 'localhost';
+$DB_PORT = "3306";
+?>
---
+++ ClothesCount/lookup.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhoupan
+ * Date: 4/23/16
+ * Time: 12:17 PM
+ */
