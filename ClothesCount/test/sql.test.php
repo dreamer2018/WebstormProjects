24a007378f36b3d6bb5083784b8722e6784ff792
dreamer2018 <zhoupans_mail@163.com>
2016-04-25 17:23:23 +0800

    v0.3

--- ClothesCount/test/sql.test.php
+++ ClothesCount/test/sql.test.php
@@ -5,3 +5,13 @@
  * Date: 4/25/16
  * Time: 5:08 PM
  */
+$name="周攀";
+$DB_TABLE_NAME="test";
+$phone=12345678910;
+$address="西安长安";
+$style=1;
+$size=2;
+$design_1=1;
+$design_2=2;
+$query = "update ".$DB_TABLE_NAME." set name=\"".$name."\",phone=".$phone.",address=\"".$address."\",style=".$style.",size=".$size.",design_1=".$design_1.",design_2=".$design_2."  where name=\"".$name."\";";
+echo $query;
