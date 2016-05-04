a1cb727656680c034a78f4eda5738ca3ad59f3b7
dreamer2018 <zhoupans_mail@163.com>
2016-05-04 09:40:55 +0800

    add click

--- Movie_Web/js/index.js
+++ Movie_Web/js/index.js
@@ -5,6 +5,13 @@ function on_click() {
 
     alert("选座成功");
 }
-function change() {
-    document.getElementsByClassName("img1").src = "image/2.jpg";
+
+function changeImage(s){
+
+    var imgObj = document.getElementById(s.id);
+    if(imgObj.getAttribute("src",2)=="image/1.jpg"){
+        imgObj.src="image/2.jpg";
+    }else{
+        imgObj.src="image/1.jpg";
+    }
 }
